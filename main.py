232e33a154556d582d56e2682fa183bf4bc4cc6a
Callum Bondy <callum.bondy@gmail.com>
2023-02-22 17:30:11 -0600

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # main file
+from recipe_scrapers import scrape_me
 
-def recipe_scraper():
-    pass
+def recipe_scraper(recipe_link):
+    scraper = scrape_me(recipe_link, wild_mode = True)
+    return scraper.ingredients()
+
+
 
 if __name__ == "__main__":
-    print("Start of program")
-    # start of our infitite loop
+    print("Welcome to Recipe Extravaganza!\n")
+    print("Here is a list of possible ways you can interact with me\n")
+    print("- Go to next or previous step\n")
+    print("- Ask questions about the current step\n")
+    print("- Ask about the ingredients or utensils\n")
+    print("If you even need a refresher on these simply type 'help' in the chat")
+    recipe = input("Please give me a link to a recipe:\n")
+    print("Holy Guacamole! That sounds yummy. To start you off here are the list of ingredients you will need:\n")
+    print(recipe_scraper(recipe))
 
     while True:
-        recipe = input("Please give me a link to a recipe:\n")
+        input = input()
+
 
