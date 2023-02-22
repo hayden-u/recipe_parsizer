fb2cd5242cc82dad07296bb1da399d123c1b18bf
Callum Bondy <callum.bondy@gmail.com>
2023-02-22 16:42:16 -0600

    main file

---
+++ main.py
@@ -0,0 +1,12 @@
+# main file
+
+def recipe_scraper():
+    pass
+
+if __name__ == "__main__":
+    print("Start of program")
+    # start of our infitite loop
+
+    while True:
+        recipe = input("Please give me a link to a recipe:\n")
+
