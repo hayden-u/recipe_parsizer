366e2fadbce0fab205c1c85f2cd4f59184fa6b55
Helena <helenaguajardo2024@u.northwestern.edu>
2023-02-22 21:23:01 -0600

    uggghhh sexy prints ingredients

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from recipe_scrapers import scrape_me
 
 def recipe_scraper(recipe_link):
     scraper = scrape_me(recipe_link, wild_mode = True)
-    return scraper.ingredients()
+    rec1 = Recipe(scraper.ingredients())
+    return rec1
 
+class Step():
+
+    def __init__(self, ingredients, utensils):
+        self.ingredients = ingredients
+        self.utensils = utensils
+
+class Recipe():
+
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
 
 
 if __name__ == "__main__":
@@ -16,7 +27,9 @@ if __name__ == "__main__":
     print("If you even need a refresher on these simply type 'help' in the chat")
     recipe = input("Please give me a link to a recipe:\n")
     print("Holy Guacamole! That sounds yummy. To start you off here are the list of ingredients you will need:\n")
-    print(recipe_scraper(recipe))
+    rec1 = recipe_scraper(recipe)
+    for i in rec1.ingredients:
+        print(i, '\n')
 
     while True:
         input = input()
