c16e30e6f9f38364c3ef52189a642dd36a6b2ccd
Callum Bondy <47482592+cbondy100@users.noreply.github.com>
2023-02-25 00:07:56 -0600

    main changes

--- main.py
+++ main.py
@@ -1,37 +1,72 @@
 # main file
 from recipe_scrapers import scrape_me
 
+# to get ingredients for each step, 
+
+
+# step class
+class recipeStep:
+    def __init__(self, step_num, step_text):
+        self.step_num = step_num
+        self.step_text = step_text
+    def __str__(self):
+        return "Step " + str(self.step_num) + ": " + self.step_text
+
 def recipe_scraper(recipe_link):
     scraper = scrape_me(recipe_link, wild_mode = True)
-    rec1 = Recipe(scraper.ingredients())
-    return rec1
-
-class Step():
-
-    def __init__(self, ingredients, utensils):
-        self.ingredients = ingredients
-        self.utensils = utensils
-
-class Recipe():
-
-    def __init__(self, ingredients):
-        self.ingredients = ingredients
+    return scraper.ingredients()
 
 
-if __name__ == "__main__":
-    print("Welcome to Recipe Extravaganza!\n")
+    for c, element in enumerate(instructions):
+        step = recipeStep(c, element)
+        steps_array.append(step)
+
+    return steps_array
+
+def recipe_ingredients(recipe_link):
+    scraper = scrape_me(recipe_link, wild_mode = True)
+    return scraper.ingredients()
+
+def printHelp():
     print("Here is a list of possible ways you can interact with me\n")
     print("- Go to next or previous step\n")
     print("- Ask questions about the current step\n")
     print("- Ask about the ingredients or utensils\n")
     print("If you even need a refresher on these simply type 'help' in the chat")
+
+def printSteps(recipe_steps):
+    for element in recipe_steps:
+        print(element)
+
+
+if __name__ == "__main__":
+    print("Welcome to Recipe Extravaganza!\n")
+    printHelp()
     recipe = input("Please give me a link to a recipe:\n")
     print("Holy Guacamole! That sounds yummy. To start you off here are the list of ingredients you will need:\n")
-    rec1 = recipe_scraper(recipe)
-    for i in rec1.ingredients:
-        print(i, '\n')
+    print(recipe_scraper(recipe))
 
+    '''
     while True:
-        input = input()
+        query = input("")
+        #chatbot exti
+        if query in exit_conditions:
+            break
+        #chatbot help
+        if query in help_conditions:
+            printHelp()
+        if query == "next":
+            if recipe_pointer >= len(recipe_steps) - 1:
+                print("We've reached the end of our recipe")
+            else:
+                recipe_pointer += 1
+                print(recipe_steps[recipe_pointer])
+        elif query == "back":
+            if recipe_pointer == 0:
+                print("Sorry we can't go back a step, we are at Step #0")
+            else:
+                recipe_pointer -= 1
+                print(recipe_steps[recipe_pointer])
+    '''
 
 
